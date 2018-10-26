d4851d8fee549c00a619000f1ebc95c108169ccf
zeyneptrl <44478760+zeyneptrl@users.noreply.github.com>
2018-10-26 19:07:15 +0300

    Güncellenmiş HelloGitHub.java
    Yeni komutu yazdım.

--- Java/HelloGitHub.java
+++ Java/HelloGitHub.java
@@ -3,9 +3,8 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 
 	    System.out.println("Hello, World!");
-	    
+	    System.out.println("Hello, GitHub!");
 	    //Yukarıdaki komutun sözdizimini kullanarak aşağıya "Hello, GitHub!" yazdırın.
-
 	}
 
 }
